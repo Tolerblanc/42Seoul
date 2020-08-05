7e8f6ab6ddfc53076f92a023a96360c2494044af
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-08-05 19:49:13 +0900

    correct strcpy

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/04 17:51:57 by hyunjuki          #+#    #+#             */
-/*   Updated: 2020/08/04 18:30:47 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2020/08/05 19:48:55 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,13 @@ char				*ft_strncpy(char *dest, char *src, unsigned int n)
 		*(dest + i) = *(src + i);
 		i++;
 	}
-	*(dest + i) = '\0';
+	if (i != n)
+	{
+		while (i < n)
+		{
+			*(dest + i) = '\0';
+			i++;
+		}
+	}
 	return (dest);
 }
