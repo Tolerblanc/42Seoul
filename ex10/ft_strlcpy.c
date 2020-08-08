cd992207d6fd23fc652f0cfec3d4c86560cd1897
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-08-08 21:15:31 +0900

    ex10

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 13:40:48 by hyunjuki          #+#    #+#             */
-/*   Updated: 2020/08/05 13:49:52 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2020/08/05 19:48:21 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@ unsigned int		ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
-	while (i < size && *(src + i) != '\0')
+	while (i < size - 1 && *(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
 	*(dest + i) = '\0';
+	while (*(src + i) != '\0')
+		i++;
 	return (i);
 }
