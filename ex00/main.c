849dea957dd08950a85992d0ec7495e7979cfb68
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-08-08 16:38:45 +0900

    main

--- ex00/main.c
+++ ex00/main.c
@@ -3,11 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyunjuki <hyunjuki@42seoul.kr>             +#+  +:+       +#+        */
+/*   By: hyunjuki <hyunjuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/08 14:11:56 by hyunjuki          #+#    #+#             */
-/*   Updated: 2020/08/08 14:11:58 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2020/08/08 16:37:41 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
 
+void			init_args(char *input, int *output);
+extern int		except(int argc, char *argv[]);
+
+int				main(int argc, char *argv[])
+{
+	int			args[16];
+
+	if (except(argc, argv))
+		return (1);
+	init_args(argv[1], args);
+
+	return (0);
+}
+
+void			init_args(char *input, int *output)
+{
+	int			i;
+
+	i = 0;
+	while (*(input + i) != '\0')
+	{
+		if (i % 2 == 0)
+			*(output + (int)(i / 2)) = *(input + i) - '0';
+		i++;
+	}
+}
