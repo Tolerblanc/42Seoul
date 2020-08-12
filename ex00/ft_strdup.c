09f003f79b206df86666de7a26e47a7dff156e3d
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-08-12 20:25:31 +0900

    add gitignore && ex00

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjuki <hyunjuki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/12 20:16:31 by hyunjuki          #+#    #+#             */
+/*   Updated: 2020/08/12 20:24:17 by hyunjuki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char		*ft_strdup(char *src)
+{
+	int		len;
+	int		i;
+	char	*str;
+
+	while (*(src + len) != '\0')
+		len++;
+	str = (char *)malloc(sizeof(char *) * len + 1);
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		*(str + i) = *(src + i);
+		i++;
+	}
+	*(str + i) = '\0';
+	return (str);
+}
