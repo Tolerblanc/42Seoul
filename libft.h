fff0e2905d42fece3fb31542e16ccdaea8695705
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-10-05 20:56:24 +0900

    init & is_functions & strlen

---
+++ libft.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjuki <hyunjuki@42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/05 19:51:34 by hyunjuki          #+#    #+#             */
+/*   Updated: 2020/10/05 20:53:01 by hyunjuki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H 
+
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isalnum(int c);
+size_t	ft_strlen(const char *s);
+
+#endif
