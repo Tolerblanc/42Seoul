fae0084ba0ecf2d1ca60a2883a25c9e20bb5e7c7
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-10-14 18:16:44 +0900

    bonus : ft_lstiter

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 19:51:34 by hyunjuki          #+#    #+#             */
-/*   Updated: 2020/10/14 18:11:36 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2020/10/14 18:16:22 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,8 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+
 
 
 #endif
