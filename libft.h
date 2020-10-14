997ef272bffcf2cbfceb4fc6fedd159cb0fb0e43
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-10-14 18:11:48 +0900

    bonus : ft_lstclear

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 19:51:34 by hyunjuki          #+#    #+#             */
-/*   Updated: 2020/10/14 17:46:44 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2020/10/14 18:11:36 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,7 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 
 #endif
