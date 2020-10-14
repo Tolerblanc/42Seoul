038b071c792d2d81bb9c86e58fac629d643ebd7f
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-10-14 17:42:47 +0900

    bonus : ft_lstadd_back

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 19:51:34 by hyunjuki          #+#    #+#             */
-/*   Updated: 2020/10/14 17:38:29 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2020/10/14 17:42:33 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new)
 
 
 #endif
