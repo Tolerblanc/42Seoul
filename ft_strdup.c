9bb7fbe6de2bea42c276209d831a78acb284be85
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-10-14 13:00:09 +0900

    ft_strdup : malloc protection

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hyunjuki <hyunjuki@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 12:34:04 by hyunjuki          #+#    #+#             */
-/*   Updated: 2020/10/14 12:38:19 by hyunjuki         ###   ########.fr       */
+/*   Updated: 2020/10/14 12:59:51 by hyunjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char		*ft_strdup(const char *s1)
 
 	i = ft_strlen(s1);
 	ret = (char *)malloc(sizeof(char) * i);
+	if (ret == 0)
+		return (ret);
 	while (*s1)
 		*ret++ = *s1++;
 	*ret = '\0';
