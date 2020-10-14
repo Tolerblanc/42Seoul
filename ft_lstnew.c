877982e11f8e464b09ff5178ce85ab8f789595c0
Hyunjun Kim <hyunjuki@student.42seoul.kr>
2020-10-14 17:30:14 +0900

    bonus : ft_lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunjuki <hyunjuki@42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/14 17:14:38 by hyunjuki          #+#    #+#             */
+/*   Updated: 2020/10/14 17:24:05 by hyunjuki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list		*ft_lstnew(void *content)
+{
+	t_list	*ret;
+
+	if (!(ret = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	ret->content = content;
+	ret->next = NULL;
+	return (ret);
+}
